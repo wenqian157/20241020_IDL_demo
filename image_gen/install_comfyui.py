6d1d9191b74a7af13d20d6df806d544095c0caca
Anton Savov <antonsavov@gmail.com>
2024-11-19 20:08:54 +0100

    Update install_comfyui.py

--- image_gen/install_comfyui.py
+++ image_gen/install_comfyui.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+
 import requests
 
 
@@ -72,64 +73,17 @@ def download_models(install_folder):
         # link:path/filename
         "https://huggingface.co/stabilityai/control-lora/resolve/main/control-LoRAs-rank256/control-lora-canny-rank256.safetensors?download=true": os.path.join(
             models_folder, "controlnet", "control-lora-canny-rank256.safetensors"
-        )
+        ),
+        "https://huggingface.co/h94/IP-Adapter/blob/main/sdxl_models/ip-adapter-plus_sdxl_vit-h.safetensors?download=true": os.path.join(
+            models_folder, "ipadapter", "ip-adapter-plus_sdxl_vit-h.safetensors"
+        ),
     }
     # Download checkpoints
     for link, path in models_dict.items():
-        download_file(link, path)
-    # download_file(
-    #     "https://civitai.com/models/139562/realvisxl-v50",
-    #     os.path.join(models_folder, "checkpoints/realvisxl-v50.safetensors"),
-    # )
-    # download_file(
-    #     "https://huggingface.co/imagepipeline/Copax-TimeLessXL-SDXL1.0/blob/main/copaxTimelessxlSDXL1_v9.safetensors",
-    #     os.path.join(models_folder, "checkpoints/copaxTimelessxlSDXL1_v9.safetensors"),
-    # )
-
-    # # Download CLIP models
-    # download_file(
-    #     "https://huggingface.co/lllyasviel/flux1_dev/blob/main/flux1-dev-fp8.safetensors",
-    #     os.path.join(models_folder, "clip/flux1-dev-fp8.safetensors"),
-    # )
-    # download_file(
-    #     "https://huggingface.co/comfyanonymous/flux_text_encoders/blob/main/clip_l.safetensors",
-    #     os.path.join(models_folder, "clip/clip_l.safetensors"),
-    # )
-
-    # # Download ControlNet models
-    # download_file(
-    #     "https://huggingface.co/stabilityai/control-lora/blob/main/control-LoRAs-rank256/control-lora-canny-rank256.safetensors",
-    #     os.path.join(
-    #         models_folder, "controlnet/control-lora-canny-rank256.safetensors"
-    #     ),
-    # )
-    # download_file(
-    #     "https://huggingface.co/InstantX/FLUX.1-dev-Controlnet-Canny/blob/main/diffusion_pytorch_model.safetensors",
-    #     os.path.join(
-    #         models_folder, "controlnet/InstantX_FLUX.1-dev-Controlnet-Canny.safetensors"
-    #     ),
-    # )
-    # download_file(
-    #     "https://huggingface.co/lllyasviel/control_v11p_sd15_normalbae/blob/main/diffusion_pytorch_model.safetensors",
-    #     os.path.join(
-    #         models_folder, "controlnet/control_v11p_sd15_normalbae.safetensors"
-    #     ),
-    # )
-
-    # # Download CLIP Vision models
-    # download_file(
-    #     "https://huggingface.co/h94/IP-Adapter/blob/main/models/image_encoder/model.safetensors",
-    #     os.path.join(
-    #         models_folder,
-    #         "clip_vision/to-use-with_ip-adapter-plus_sdxl_vit-h.safetensors",
-    #     ),
-    # )
-
-    # # Download IP Adapter models
-    # download_file(
-    #     "https://huggingface.co/h94/IP-Adapter/blob/main/sdxl_models/ip-adapter-plus_sdxl_vit-h.safetensors",
-    #     os.path.join(models_folder, "ipadapter/ip-adapter-plus_sdxl_vit-h.safetensors"),
-    # )
+        if not os.path.exists(path):
+            download_file(link, path)
+        else:
+            print(f"File already exists: {path}")
 
 
 # Run installation functions
