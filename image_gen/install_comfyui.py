2f0b78486c005e47cd4c98a89caff786ffb3af75
Anton Savov <antonsavov@gmail.com>
2024-11-19 19:33:21 +0100

    Update install_comfyui.py

--- image_gen/install_comfyui.py
+++ image_gen/install_comfyui.py
@@ -2,13 +2,15 @@ import os
 import subprocess
 import requests
 
+
 def download_file(url, dest):
     print(f"Downloading {url}...")
     response = requests.get(url)
     response.raise_for_status()
-    with open(dest, 'wb') as file:
+    with open(dest, "wb") as file:
         file.write(response.content)
     print(f"Downloaded {url} to {dest}")
+
 
 # Create necessary directories
 def create_directory(path):
@@ -18,6 +20,7 @@ def create_directory(path):
     else:
         print(f"Directory already exists: {path}")
 
+
 # Install Manager and Extensions
 def install_manager_and_extensions(install_folder):
     custom_nodes_folder = os.path.join(install_folder, "ComfyUI/custom_nodes")
@@ -26,70 +29,121 @@ def install_manager_and_extensions(install_folder):
         return
     os.chdir(custom_nodes_folder)
     custom_extensions = [
-        'https://github.com/ltdrdata/ComfyUI-Manager',
-        'https://github.com/Fannovel16/comfyui_controlnet_aux',
-        'https://github.com/BadCafeCode/masquerade-nodes-comfyui',
-        'https://github.com/cubiq/ComfyUI_essentials',
-        'https://github.com/cubiq/ComfyUI_IPAdapter_plus',
-        'https://github.com/rgthree/rgthree-comfy',
-        'https://github.com/Lerc/canvas_tab',
-        'https://github.com/chrisgoringe/cg-use-everywhere',
-        'https://github.com/shadowcz007/comfyui-mixlab-nodes',
-        'https://github.com/palant/image-resize-comfyui',
-        'https://github.com/GraftingRayman/ComfyUI_GraftingRayman',
-        'https://github.com/ai-shizuka/ComfyUI-tbox',
-        
+        "https://github.com/ltdrdata/ComfyUI-Manager",
+        "https://github.com/Fannovel16/comfyui_controlnet_aux",
+        "https://github.com/BadCafeCode/masquerade-nodes-comfyui",
+        "https://github.com/cubiq/ComfyUI_essentials",
+        "https://github.com/cubiq/ComfyUI_IPAdapter_plus",
+        "https://github.com/rgthree/rgthree-comfy",
+        "https://github.com/Lerc/canvas_tab",
+        "https://github.com/chrisgoringe/cg-use-everywhere",
+        "https://github.com/shadowcz007/comfyui-mixlab-nodes",
+        "https://github.com/palant/image-resize-comfyui",
+        "https://github.com/GraftingRayman/ComfyUI_GraftingRayman",
+        "https://github.com/ai-shizuka/ComfyUI-tbox",
     ]
 
     for repo_url in custom_extensions:
-        repo_name = repo_url.split('/')[-1]
+        repo_name = repo_url.split("/")[-1]
         if os.path.exists(repo_name):
             print(f"Repository {repo_name} already exists. Fetching latest changes...")
             os.chdir(repo_name)
-            subprocess.run(['git', 'fetch'], check=True)
-            subprocess.run(['git', 'pull'], check=True)
-            os.chdir('..')
+            subprocess.run(["git", "fetch"], check=True)
+            subprocess.run(["git", "pull"], check=True)
+            os.chdir("..")
         else:
-            subprocess.run(['git', 'clone', repo_url], check=True)
+            subprocess.run(["git", "clone", repo_url], check=True)
             print(f"Cloned {repo_url}")
+
 
 # Download models and put them in the appropriate folders
 def download_models(install_folder):
     models_folder = os.path.join(install_folder, "ComfyUI/models")
+    if not os.path.exists(models_folder):
+        print(f"Directory does not exist: {models_folder}")
+        return
     create_directory(os.path.join(models_folder, "checkpoints"))
     create_directory(os.path.join(models_folder, "clip"))
     create_directory(os.path.join(models_folder, "controlnet"))
     create_directory(os.path.join(models_folder, "clip_vision"))
     create_directory(os.path.join(models_folder, "ipadapter"))
 
+    models_dict = {
+        # link:path/filename
+        "https://huggingface.co/stabilityai/control-lora/resolve/main/control-LoRAs-rank256/control-lora-canny-rank256.safetensors?download=true": os.path.join(
+            models_folder, "controlnet", "control-lora-canny-rank256.safetensors"
+        )
+    }
     # Download checkpoints
-    download_file("https://civitai.com/models/139562/realvisxl-v50", os.path.join(models_folder, "checkpoints/realvisxl-v50.safetensors"))
-    download_file("https://huggingface.co/imagepipeline/Copax-TimeLessXL-SDXL1.0/blob/main/copaxTimelessxlSDXL1_v9.safetensors", os.path.join(models_folder, "checkpoints/copaxTimelessxlSDXL1_v9.safetensors"))
+    for link, path in models_dict.items():
+        download_file(link, path)
+    # download_file(
+    #     "https://civitai.com/models/139562/realvisxl-v50",
+    #     os.path.join(models_folder, "checkpoints/realvisxl-v50.safetensors"),
+    # )
+    # download_file(
+    #     "https://huggingface.co/imagepipeline/Copax-TimeLessXL-SDXL1.0/blob/main/copaxTimelessxlSDXL1_v9.safetensors",
+    #     os.path.join(models_folder, "checkpoints/copaxTimelessxlSDXL1_v9.safetensors"),
+    # )
 
-    # Download CLIP models
-    download_file("https://huggingface.co/lllyasviel/flux1_dev/blob/main/flux1-dev-fp8.safetensors", os.path.join(models_folder, "clip/flux1-dev-fp8.safetensors"))
-    download_file("https://huggingface.co/comfyanonymous/flux_text_encoders/blob/main/clip_l.safetensors", os.path.join(models_folder, "clip/clip_l.safetensors"))
+    # # Download CLIP models
+    # download_file(
+    #     "https://huggingface.co/lllyasviel/flux1_dev/blob/main/flux1-dev-fp8.safetensors",
+    #     os.path.join(models_folder, "clip/flux1-dev-fp8.safetensors"),
+    # )
+    # download_file(
+    #     "https://huggingface.co/comfyanonymous/flux_text_encoders/blob/main/clip_l.safetensors",
+    #     os.path.join(models_folder, "clip/clip_l.safetensors"),
+    # )
 
-    # Download ControlNet models
-    download_file("https://huggingface.co/stabilityai/control-lora/blob/main/control-LoRAs-rank256/control-lora-canny-rank256.safetensors", os.path.join(models_folder, "controlnet/control-lora-canny-rank256.safetensors"))
-    download_file("https://huggingface.co/InstantX/FLUX.1-dev-Controlnet-Canny/blob/main/diffusion_pytorch_model.safetensors", os.path.join(models_folder, "controlnet/InstantX_FLUX.1-dev-Controlnet-Canny.safetensors"))
-    download_file("https://huggingface.co/lllyasviel/control_v11p_sd15_normalbae/blob/main/diffusion_pytorch_model.safetensors", os.path.join(models_folder, "controlnet/control_v11p_sd15_normalbae.safetensors"))
+    # # Download ControlNet models
+    # download_file(
+    #     "https://huggingface.co/stabilityai/control-lora/blob/main/control-LoRAs-rank256/control-lora-canny-rank256.safetensors",
+    #     os.path.join(
+    #         models_folder, "controlnet/control-lora-canny-rank256.safetensors"
+    #     ),
+    # )
+    # download_file(
+    #     "https://huggingface.co/InstantX/FLUX.1-dev-Controlnet-Canny/blob/main/diffusion_pytorch_model.safetensors",
+    #     os.path.join(
+    #         models_folder, "controlnet/InstantX_FLUX.1-dev-Controlnet-Canny.safetensors"
+    #     ),
+    # )
+    # download_file(
+    #     "https://huggingface.co/lllyasviel/control_v11p_sd15_normalbae/blob/main/diffusion_pytorch_model.safetensors",
+    #     os.path.join(
+    #         models_folder, "controlnet/control_v11p_sd15_normalbae.safetensors"
+    #     ),
+    # )
 
-    # Download CLIP Vision models
-    download_file("https://huggingface.co/h94/IP-Adapter/blob/main/models/image_encoder/model.safetensors", os.path.join(models_folder, "clip_vision/to-use-with_ip-adapter-plus_sdxl_vit-h.safetensors"))
+    # # Download CLIP Vision models
+    # download_file(
+    #     "https://huggingface.co/h94/IP-Adapter/blob/main/models/image_encoder/model.safetensors",
+    #     os.path.join(
+    #         models_folder,
+    #         "clip_vision/to-use-with_ip-adapter-plus_sdxl_vit-h.safetensors",
+    #     ),
+    # )
 
-    # Download IP Adapter models
-    download_file("https://huggingface.co/h94/IP-Adapter/blob/main/sdxl_models/ip-adapter-plus_sdxl_vit-h.safetensors", os.path.join(models_folder, "ipadapter/ip-adapter-plus_sdxl_vit-h.safetensors"))
+    # # Download IP Adapter models
+    # download_file(
+    #     "https://huggingface.co/h94/IP-Adapter/blob/main/sdxl_models/ip-adapter-plus_sdxl_vit-h.safetensors",
+    #     os.path.join(models_folder, "ipadapter/ip-adapter-plus_sdxl_vit-h.safetensors"),
+    # )
+
 
 # Run installation functions
 def main():
-    install_folder = "D:\\Anton\\ComfyUI_windows_portable_nvidia\\ComfyUI_windows_portable_nvidia"
+    install_folder = (
+        "D:\\Anton\\ComfyUI_windows_portable_nvidia\\ComfyUI_windows_portable_nvidia"
+    )
     if not os.path.exists(install_folder):
         print(f"Installation folder does not exist: {install_folder}")
         return
 
     install_manager_and_extensions(install_folder)
     # download_models(install_folder)
+
 
 if __name__ == "__main__":
     main()
